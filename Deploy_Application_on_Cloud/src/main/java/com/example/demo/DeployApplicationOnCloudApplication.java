846a70b88fec648cf40906baa8690810489a6aae
Prasanth B <prasanthbaskaran0252000@gmail.com>
2022-10-19 15:11:05 +0530

    deploy on cloud

---
+++ Deploy_Application_on_Cloud/src/main/java/com/example/demo/DeployApplicationOnCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployApplicationOnCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployApplicationOnCloudApplication.class, args);
+	}
+
+}
